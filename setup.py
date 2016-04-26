37eaf321b8bb25e54e822f2f493df8804fe5a429
Maxime Gaudin <gaudin.maxime@gmail.com>
2016-04-26 08:47:15 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
         name='mongo-connector-postgresql',
-        version='1.0-beta18',
+        version='1.0-beta19',
         description='Doc Manager Postgresl for Mongo connector Distribution Utilities',
         keywords=['mongo-connector', 'mongo', 'mongodb', 'postgresql'],
         platforms=["any"],
