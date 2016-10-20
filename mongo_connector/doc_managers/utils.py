71e304c7460791ce7919ff7cfe68ef38b380f736
Maxime Gaudin <gaudin.maxime@gmail.com>
2016-10-20 15:27:13 +0200

    [Connector] Corner-cases

--- mongo_connector/doc_managers/utils.py
+++ mongo_connector/doc_managers/utils.py
@@ -75,11 +75,14 @@ def get_foreign_key(mappings, db, collection, field):
 
 
 def get_nested_field_from_document(document, dot_notation_key):
+    if document is None:
+        return None
+
     if '.' not in dot_notation_key:
         return document.get(dot_notation_key, None)
 
     partial_key = dot_notation_key.split('.')[0]
-    if not partial_key or partial_key not in document:
+    if not isinstance(document, dict) or partial_key not in document:
         return None
 
     return get_nested_field_from_document(document[partial_key], '.'.join(dot_notation_key.split('.')[1:]))
